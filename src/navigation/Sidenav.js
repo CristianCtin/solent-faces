5083c34486df027d95d8e02d9450533b4d5ad2dd
CristianCtin <constantincristian@gmail.com>
2024-01-04 10:40:42 +0000

    Menu buttons
    Menu buttons created

--- src/navigation/Sidenav.js
+++ src/navigation/Sidenav.js
@@ -1,6 +1,13 @@
 import React from 'react';
 import "./Sidenav.css";
-
+import HomeIcon from '@mui/icons-material/Home';
+import SearchIcon from '@mui/icons-material/Search';
+import ExploreIcon from '@mui/icons-material/Explore';
+import SlideshowIcon from '@mui/icons-material/Slideshow';
+import ChatIcon from '@mui/icons-material/Chat';
+import FavoriteBorderIcon from '@mui/icons-material/FavoriteBorder';
+import AddCircleOutlineIcon from '@mui/icons-material/AddCircleOutline';
+import MenuIcon from '@mui/icons-material/Menu';
 
 function Sidenav() {
   return <div className='sidenav'>
@@ -8,10 +15,48 @@ function Sidenav() {
     alt='' 
     />
     <div className='sidenav__buttons'>
-        <div className='side__button'>
-            {/* Icons */}
-            <span>Home</span>
-        </div>
+        <button className='sidenav__button'>
+            <HomeIcon />
+            <span>Home</span> 
+        </button>
+
+        <button className='sidenav__button'>
+          <SearchIcon />
+            <span>Search</span> 
+        </button>
+
+        <button className='sidenav__button'>
+            <ExploreIcon />
+            <span>Explore</span>
+        </button>
+
+        <button className='sidenav__button'>
+            <SlideshowIcon />
+            <span>Shorts</span>
+        </button>
+
+        <button className='sidenav__button'>
+            <ChatIcon />
+            <span>Messages</span>
+        </button>
+
+        <button className='sidenav__button'>
+            <FavoriteBorderIcon />
+            <span>Notifications</span>
+        </button>
+
+        <button className='sidenav__button'>
+            <AddCircleOutlineIcon />
+            <span>Create</span>
+        </button>
+
+        
+    </div>
+    <div className='sidenav__more'>
+      <button className='sidenav__button'>
+        <MenuIcon />
+        <span>More</span>
+      </button>
     </div>
   </div>
   
