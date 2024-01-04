be103d1f92eeaa062c861b77502064aa2c487203
CristianCtin <constantincristian@gmail.com>
2024-01-04 10:03:49 +0000

    Sidenav
    Sidenav created

--- src/navigation/Sidenav.js
+++ src/navigation/Sidenav.js
@@ -3,9 +3,18 @@ import "./Sidenav.css";
 
 
 function Sidenav() {
-  return (
-    <div>Sidenav</div>
-  )
+  return <div className='sidenav'>
+    <img className='sidenav__logo' src='https://th.bing.com/th/id/OIG.lH9fj81ayjsGuUNzLS7d?pid=ImgGn' 
+    alt='' 
+    />
+    <div className='sidenav__buttons'>
+        <div className='side__button'>
+            {/* Icons */}
+            <span>Home</span>
+        </div>
+    </div>
+  </div>
+  
 }
 
 export default Sidenav
